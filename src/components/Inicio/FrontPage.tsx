1a41e0610269331ebe43165479338295157bdcea
AcuacheY <v.acuache15@gmail.com>
2025-07-29 16:08:26 -0500

    style(FrontPage): Modify button position and title thickness

--- src/components/Inicio/FrontPage.tsx
+++ src/components/Inicio/FrontPage.tsx
@@ -28,11 +28,11 @@ export default function FrontPage() {
     <main className="w-full  min-h-dvh flex flex-col" style={{ background: 'var(--gradient-st)' }}>
       <div className=" flex-1 basis-[90%] px-4 max-w-6xl m-auto text-white text-center flex flex-col gap-10 items-center py-10 pb-15 md:px-8 sm:py-30 sm:flex-row sm:text-left sm:gap-12 md:gap-18 lg:gap-25 xl:px-0">
         <div className="flex flex-col gap-8 sm:w-1/3 lg:w-1/3">
-          <h1 className="text-4xl font-bold sm:text-4xl md:text-5xl lg:text-6xl text-balance">Tranformamos vidas con el poder de la educación</h1>
+          <h1 className="text-4xl font-bold sm:text-4xl md:text-5xl lg:text-6xl text-balance" style={{ textShadow: '0 0 1px currentColor, 1px 0 1px currentColor, 0 1px 1px currentColor, -1px 0 1px currentColor' }}>Tranformamos vidas con el poder de la educación</h1>
           <p className="sm:text-base md:text-xl lg:text-xl">Brindamos talleres gratuitos, cursos en nuestra aula virtual y proyectos educativos con impacto solial para estudiantes y docentes.</p>
-          <div className="flex gap-5 w-fit m-auto sm:m-0">
-            <button className="text-sm md:text-base lg:text-lg border-2 border-white px-5 py-2 rounded-4xl bg-nd">Donar Ahora</button>
-            <button className="text-sm md:text-base lg:text-lg border-2 border-white px-5 py-2 rounded-4xl bg-st">Ir al Aula Virtual</button>
+          <div className="flex gap-5 w-fit m-auto sm:m-0 sm:relative sm:top-9 md:top-0">
+            <button className="whitespace-nowrap text-sm md:text-sm lg:text-lg border-2 border-white px-5 py-2 rounded-4xl bg-nd">Donar Ahora</button>
+            <button className="whitespace-nowrap text-sm md:text-sm lg:text-lg border-2 border-white px-5 py-2 rounded-4xl bg-st">Ir al Aula Virtual</button>
           </div>
         </div>
         <div className={`p-3 rounded-3xl w-full h-100 sm:w-2/3 lg:w-2/3 relative z-2 transition-all duration-700 ease-out ${isAnimating
@@ -40,7 +40,7 @@ export default function FrontPage() {
           : 'transform translate-y-0 rotate-0 scale-100'
           }`}>
           {/* Marco de fondo */}
-          <div className={`tranform scale-90 sm:scale-100 w-[calc(100%+12px)] h-[calc(100%+4px)] -top-3 -left-1 absolute sm:w-[calc(100%-10px)] sm:h-[calc(100%-10px)] bg-gray-200 sm:-top-1 sm:-left-1 rounded-3xl -z-1 transition-all duration-700 ease-out ${isAnimating
+          <div className={`tranform scale-90 sm:scale-100 w-[calc(100%+12px)] h-[calc(100%+4px)] -top-3 -left-1 absolute sm:w-[calc(100%-10px)] sm:h-[calc(100%-10px)] bg-gray-200 sm:top-2 sm:-left-1 rounded-3xl -z-1 transition-all duration-700 ease-out ${isAnimating
             ? 'rotate-[8deg] translate-y-2'
             : 'rotate-[4deg] sm:rotate-[5deg] translate-y-0'
             }`}></div>
