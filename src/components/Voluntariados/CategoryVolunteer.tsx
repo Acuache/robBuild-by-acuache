3c244bb69af44b4417b4dfbdda51ad038418625c
AcuacheY <v.acuache15@gmail.com>
2025-07-31 20:50:21 -0500

    feat(VolunteersSection): Filter logic and by category applied

--- src/components/Voluntariados/CategoryVolunteer.tsx
+++ src/components/Voluntariados/CategoryVolunteer.tsx
@@ -1,20 +1,29 @@
 interface CategoryVolunteerProps {
   children: React.ReactNode,
   number: number,
-  title: string
+  title: string,
+  onClick: () => void,
+  isActive: boolean
 }
 
-export default function CategoryVolunteer({ children, number, title }: CategoryVolunteerProps) {
+export default function CategoryVolunteer({ children, number, title, onClick, isActive }: CategoryVolunteerProps) {
   return (
     <button
+      onClick={onClick}
       style={{ boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.25)" }}
-      className="category-btn cursor-pointer flex gap-2 items-center border-[1px] border-black rounded-md px-5 py-2 bg-white shadow-2xl hover:text-white transition-all "
+      className={`category-btn cursor-pointer flex gap-2 items-center border-[1px] rounded-md px-5 py-2 shadow-2xl transition-all ${isActive
+          ? 'bg-gradient-to-r from-[#33AA6F] to-[#2AA099] text-white border-transparent'
+          : 'bg-white border-gray-200 hover:text-white'
+        }`}
     >
       <div>
         {children}
       </div>
       <p className="font-semibold">{title}</p>
-      <p className="text-st p-1 bg-[#d9f1e9] rounded-full w-8 h-8 font-bold hover:text-white hover:bg-white/20 transition-all duration-300">{number}</p>
+      <p className={`p-1 rounded-full w-8 h-8 font-bold transition-all duration-300 ${isActive
+          ? 'text-white bg-white/20'
+          : 'text-st bg-[#d9f1e9] hover:text-white hover:bg-white/20'
+        }`}>{number}</p>
     </button>
   )
 }
--- src/components/Voluntariados/VolunteersSection.tsx
+++ src/components/Voluntariados/VolunteersSection.tsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
 import volunteers from '../../data/api-volunteers.json'
 import { CardVolunteer, CategoryVolunteer } from './'
 import { Icon } from "@iconify/react";
+
 export default function VolunteersSection() {
+  const [category, setCategory] = useState<string>("Todos")
+
+  // Función para randomizar array 
+  const shuffleArray = (array: any[]) => {
+    const newArray = [...array]
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+    }
+    return newArray
+  }
+
+  const filterCategory = () => {
+    let filtered
+    if (category === "Todos") {
+      filtered = volunteers
+    } else {
+      filtered = volunteers.filter((volunteer) => volunteer.specialty === category)
+    }
+
+    return shuffleArray(filtered)
+  }
+
+  // Volunters de manera Aleatorio
+  const filterVolunteer = filterCategory()
+
   return (
     <section className="w-full  py-15  bg-[#f4fbf8]">
       <div className='px-5 max-w-6xl m-auto flex flex-col gap-8 lg:gap-11 lg:px-0'>
@@ -16,36 +44,48 @@ export default function VolunteersSection() {
           <CategoryVolunteer
             title="Todos los voluntarios"
             number={50}
+            onClick={() => setCategory("Todos")}
+            isActive={category === "Todos"}
           >
             <Icon icon="formkit:people" width="25" height="25" />
           </CategoryVolunteer>
           <CategoryVolunteer
             title="Programación"
-            number={50}
+            number={12}
+            onClick={() => setCategory("Programación")}
+            isActive={category === "Programación"}
           >
             <Icon icon="fa:code" width="26" height="20" />
           </CategoryVolunteer>
           <CategoryVolunteer
             title="Marketing"
-            number={50}
+            number={8}
+            onClick={() => setCategory("Marketing")}
+            isActive={category === "Marketing"}
           >
             <Icon icon="mdi:megaphone" width="24" height="24" />
           </CategoryVolunteer>
           <CategoryVolunteer
-            title="Electronica"
-            number={50}
+            title="Electrónica"
+            number={17}
+            onClick={() => setCategory("Electrónica")}
+            isActive={category === "Electrónica"}
           >
             <Icon icon="mage:electricity-fill" width="24" height="24" />
           </CategoryVolunteer>
           <CategoryVolunteer
             title="Embajadores"
-            number={50}
+            number={3}
+            onClick={() => setCategory("Embajadores")}
+            isActive={category === "Embajadores"}
           >
             <Icon icon="fa-solid:handshake" width="25" height="20" />
           </CategoryVolunteer>
           <CategoryVolunteer
             title="Mecánica"
-            number={50}
+            number={10}
+            onClick={() => setCategory("Mecánica")}
+            isActive={category === "Mecánica"}
           >
             <Icon icon="bxs:wrench" width="24" height="24" />
           </CategoryVolunteer>
@@ -53,7 +93,7 @@ export default function VolunteersSection() {
 
         <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-8 justify-items-center'>
           {
-            volunteers.map((volunteer, index) => (
+            filterVolunteer.map((volunteer, index) => (
               <CardVolunteer
                 {...volunteer}
                 key={index}
