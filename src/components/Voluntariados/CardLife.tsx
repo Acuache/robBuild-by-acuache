ceecf9bb51e95df986341751201be417b0d019c1
AcuacheY <v.acuache15@gmail.com>
2025-08-02 04:22:50 -0500

    style(CardLife): Cards responsive and add button

--- src/components/Voluntariados/CardLife.tsx
+++ src/components/Voluntariados/CardLife.tsx
@@ -2,15 +2,17 @@ interface CardLifeProps {
   title: string,
   description: string,
   children: React.ReactNode
+  colorBg: string,
 }
-export default function CardLife({ title, description, children }: CardLifeProps) {
+export default function CardLife({ title, description, children, colorBg }: CardLifeProps) {
   return (
-    <article className="flex flex-col">
-      <div>
+    <article className=" group bg-white backdrop-blur-sm rounded-lg p-6 flex flex-col gap-4 text-black w-full items-center justify-center h-55 transition-all duration-300 ease-out hover:-translate-y-1 hover:shadow-lg hover:shadow-black/20 cursor-pointer"
+      style={{ '--hover-bg': colorBg } as React.CSSProperties & { '--hover-bg': string }}>
+      <div style={{ backgroundColor: colorBg }} className="rounded-lg flex justify-center items-center p-3 transition-all duration-300">
         {children}
       </div>
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <h3 className="text-center text-xl font-bold">{title}</h3>
+      <p className="text-sm opacity-90 text-center">{description}</p>
     </article>
   )
 }
--- src/components/Voluntariados/LifeSection.tsx
+++ src/components/Voluntariados/LifeSection.tsx
@@ -2,8 +2,8 @@ import { Icon } from "@iconify/react";
 import { CardLife } from './'
 export default function LifeSection() {
   return (
-    <section className="w-full text-white py-15 sm:py-15" style={{ background: 'var(--gradient-st)' }}>
-      <div className='px-5 max-w-6xl m-auto flex flex-col gap-8 lg:gap-11 lg:px-0'>
+    <section className="mb-15 w-full text-white py-15 sm:py-15" style={{ background: 'var(--gradient-st)' }}>
+      <div className='px-5 max-w-6xl m-auto flex flex-col gap-8 lg:gap-11 xl:px-0'>
 
         {/* Header de la sección */}
         <header className='flex flex-col gap-5 items-center'>
@@ -12,33 +12,48 @@ export default function LifeSection() {
           <p className='description text-pretty'>Únete a profesionales apasionados construyendo el futuro STEM del Perú</p>
         </header>
 
-        <section className=''>
+        <section className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 '>
           <CardLife
             title="Aprende y crece"
             description='Aprende metodologías activas y usa herramientas digitales innovadoras.'
+            colorBg="#eff6ff"
           >
-            <Icon icon="uil:statistics" width="24" height="24" />
+            <Icon className="text-[#2563eb]" icon="uil:statistics" width="30" height="30" />
           </CardLife>
           <CardLife
             title="Horarios flexibles"
             description='Presencial, virtual o fines de semana. Tú decides cuándo participar.'
+            colorBg="#f0fdf4"
           >
-            <Icon icon="flowbite:clock-outline" width="24" height="24" />
+            <Icon className="text-[#16a34a]" icon="flowbite:clock-outline" width="30" height="30" />
           </CardLife>
           <CardLife
             title="Certificación oficial"
             description='Obtén un certificado válido para tu CV, LinkedIn y nuevas oportunidades.'
+            colorBg="#faf5ff"
           >
-            <Icon icon="iconoir:medal" width="24" height="24" />
+            <Icon className="text-[#9333ea]" icon="iconoir:medal" width="30" height="30" />
           </CardLife>
           <CardLife
             title="Impacto real"
             description='Forma líderes del mañana con robótica educativa y enfoque STEAM.'
+            colorBg="#fcf0f0"
           >
-            <Icon icon="mdi:heart-outline" width="24" height="24" />
+            <Icon className="text-[#dc2626]" icon="mdi:heart-outline" width="30" height="30" />
           </CardLife>
         </section>
 
+        <button className=" cursor-pointer m-auto bg-white text-st flex w-fit items-center justify-center gap-3 px-5 py-3 rounded-lg">
+          <span>
+            <Icon icon="mdi:heart-outline" width="26" height="26" />
+          </span>
+          <span className="font-bold w-auto">
+            !Quiero ser Voluntario¡
+          </span>
+          <span>
+            <Icon icon="proicons:arrow-right" width="26" height="26" />
+          </span>
+        </button>
       </div>
     </section>
   )
