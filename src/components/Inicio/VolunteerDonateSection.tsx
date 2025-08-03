d0b5bdc0b93657d58e378c30d79c537b019a8448
AcuacheY <v.acuache15@gmail.com>
2025-08-03 18:17:09 -0500

    feat(VolunteerDonateSection): Added button logic to go to its specific section

--- src/components/Inicio/VolunteerDonateSection.tsx
+++ src/components/Inicio/VolunteerDonateSection.tsx
@@ -1,5 +1,5 @@
 import { Icon } from "@iconify/react";
-
+import { Link } from 'react-router-dom'
 export default function VolunteerDonateSection() {
   return (
     <section className="w-full text-white py-15 sm:py-15" style={{ background: 'var(--gradient-st)' }}>
@@ -18,8 +18,12 @@ export default function VolunteerDonateSection() {
         </header>
 
         <section className='flex flex-row flex-wrap justify-center items-center gap-6 sm:gap-15'>
-          <button className="w-50 font-semibold text-base bg-white text-st px-10 py-3 rounded-lg transition-all duration-300 hover:shadow-2xl hover:shadow-white/40 hover:-translate-y-1">Ser voluntario</button>
-          <button className="w-fit font-semibold text-base bg-white text-st px-10 py-3 rounded-lg transition-all duration-300 hover:shadow-2xl hover:shadow-white/40 hover:-translate-y-1">Hacer Donación</button>
+          <Link to="/voluntariados">
+            <button className="w-50 font-semibold text-base bg-white text-st px-10 py-3 rounded-lg transition-all duration-300 hover:shadow-2xl hover:shadow-white/40 hover:-translate-y-1">Ser voluntario</button>
+          </Link>
+          <Link to="donar">
+            <button className="w-fit font-semibold text-base bg-white text-st px-10 py-3 rounded-lg transition-all duration-300 hover:shadow-2xl hover:shadow-white/40 hover:-translate-y-1">Hacer Donación</button>
+          </Link>
         </section>
 
       </div>
