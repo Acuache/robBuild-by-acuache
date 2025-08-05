ef5f53fc8e15a52b8a1349c72819a8d27728e3e8
AcuacheY <v.acuache15@gmail.com>
2025-08-04 23:33:18 -0500

    style(CardIcon): Styles Change font size and rotation

--- src/components/Talleres/CardIcon.tsx
+++ src/components/Talleres/CardIcon.tsx
@@ -32,11 +32,11 @@ export default function CardIcon({ children, number, description, isMayor500 }: 
 
   return (
     <article ref={ref} className="flex flex-col justify-between items-center gap-1 min-w-50 w-full max-w-70 p-4 rounded-xl transition-all duration-300 ease-in-out hover:scale-102 hover:shadow-md hover:bg-white active:scale-102 active:shadow-md active:bg-white cursor-pointer group">
-      <div className="bg-st flex items-center justify-center p-4 rounded-full text-white mb-3 h-18 w-18 transition-all duration-300 group-hover:scale-105 group-hover:rotate-3 group-active:scale-105 group-active:rotate-3">
+      <div className="bg-st flex items-center justify-center p-4 rounded-full text-white mb-3 h-18 w-18 transition-all duration-300 group-hover:scale-105 group-active:scale-105 ">
         {children}
       </div>
-      <h3 className="text-5xl font-bold transition-all duration-300 group-hover:text-st group-active:text-st">{accelerator}<span>+</span></h3>
-      <p className="text-xl text-gray800 transition-all duration-300 group-hover:text-gray-600 group-active:text-gray-600">{description}</p>
+      <h3 className="text-4xl font-bold transition-all duration-300 group-hover:text-st group-active:text-st">{accelerator}<span>+</span></h3>
+      <p className="text-lg text-gray800 transition-all duration-300 group-hover:text-gray-600 group-active:text-gray-600">{description}</p>
     </article>
   )
 }
