a043f1daaa13f8c68e56a40994c0bd497e50b531
AcuacheY <v.acuache15@gmail.com>
2025-08-06 03:48:15 -0500

    feat(Noticias): Component footer add

--- src/pages/Noticias.tsx
+++ src/pages/Noticias.tsx
@@ -1,8 +1,10 @@
 import { FrontPageNoticia } from '../components/Noticias'
+import { Footer } from '../components/ui'
 export default function Noticias() {
   return (
     <>
       <FrontPageNoticia />
+      <Footer />
     </>
   )
 }
