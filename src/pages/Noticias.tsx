b7c066fbd6f2c099bd0d78917d6cdb26842327fa
AcuacheY <v.acuache15@gmail.com>
2025-08-06 04:01:06 -0500

    feat(Noticias): Add scroll 0

--- src/pages/Noticias.tsx
+++ src/pages/Noticias.tsx
@@ -1,6 +1,11 @@
 import { FrontPageNoticia } from '../components/Noticias'
 import { Footer } from '../components/ui'
+import { useEffect } from 'react'
 export default function Noticias() {
+  // Scroll al top cuando se monta el componente
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <>
       <FrontPageNoticia />
