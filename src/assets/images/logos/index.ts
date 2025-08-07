fbf5d116505d86b41ffddc654afb6ed9455673c1
AcuacheY <v.acuache15@gmail.com>
2025-08-06 22:41:13 -0500

    chore: add favicon to the project

--- src/assets/images/logos/index.ts
+++ src/assets/images/logos/index.ts
@@ -1,3 +1,4 @@
 // Exportar las imágenes para que se puedan usar desde otros archivos
 export {default as logoColor} from "./complete-color.png"
 export {default as logoWhite} from "./complete-white.png"
+export {default as logotipoColor} from "./logotipoColor.png"
