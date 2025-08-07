7b8033c9ffffe85b8eb51a8c72212703748af03e
AcuacheY <v.acuache15@gmail.com>
2025-08-07 16:30:43 -0500

    style(FrontPage): Real images added

--- src/assets/images/frontPage/index.ts
+++ src/assets/images/frontPage/index.ts
@@ -1,9 +1,8 @@
-import luffy1 from "./luffy1.png"
-import luffy2 from "./luffy2.png"
-import luffy3 from "./luffy3.png"
+import home1 from "./home1.png"
+import home2 from "./home2.png"
 
 // Exporta un array con todas las imágenes
-export const frontPageImages = [luffy1, luffy2, luffy3]
+export const frontPageImages = [home1, home2]
 
 // También exporta individualmente por si las necesitas
-export { luffy1, luffy2, luffy3 }
+export { home1, home2 }
