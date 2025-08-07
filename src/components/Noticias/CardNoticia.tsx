318ec2b7207605943cec0f3233608dd9ae2ca2ca
AcuacheY <v.acuache15@gmail.com>
2025-08-06 21:45:40 -0500

    style(CardNoticia): Added shading effect

--- src/components/Noticias/CardNoticia.tsx
+++ src/components/Noticias/CardNoticia.tsx
@@ -11,7 +11,7 @@ interface CardNoticiaProps {
 export default function CardNoticia({ url, title, img, date, isVisible }: CardNoticiaProps) {
   const img__ciplima = (img === "ciplima.png") ? "w-[75%]" : "w-[55%]"
   return (
-    <article className={`bg-white flex flex-col h-full overflow-hidden rounded-xl relative transition-all duration-700 ease-out transform ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-8"}`}>
+    <article className={`bg-white flex flex-col h-full overflow-hidden rounded-xl relative transition-all duration-700 ease-out transform hover:shadow-lg hover:shadow-gray-300/50 hover:-translate-y-1 ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-8"}`}>
       <div className="h-40 bg-gray-100 flex items-center justify-center">
         <img className={`${img__ciplima} h-full object-contain`} src={getVolunteerImage(img)} alt={title} />
       </div>
