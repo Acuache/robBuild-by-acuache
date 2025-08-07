89a5d9f1217bdfb8bb8f748600a94049cdd095e1
AcuacheY <v.acuache15@gmail.com>
2025-08-06 19:07:55 -0500

    feat(VolunteerDonateSection): Absolute path corrected

--- src/components/Inicio/VolunteerDonateSection.tsx
+++ src/components/Inicio/VolunteerDonateSection.tsx
@@ -18,7 +18,7 @@ export default function VolunteerDonateSection() {
         </header>
 
         <section className='flex flex-row flex-wrap justify-center items-center gap-6 sm:gap-15'>
-          <Link to="/voluntariados">
+          <Link to="/voluntarios">
             <button className="w-50 font-semibold text-base bg-white text-st px-10 py-3 rounded-lg transition-all duration-300 hover:shadow-2xl hover:shadow-white/40 hover:-translate-y-1">Ser voluntario</button>
           </Link>
           <Link to="donar">
