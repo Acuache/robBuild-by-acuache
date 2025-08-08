d146d908104116418399a7f47274f68fd7705998
AcuacheY <v.acuache15@gmail.com>
2025-08-07 19:08:39 -0500

    style(FrontPage): Corrected spelling

--- src/components/Inicio/FrontPage.tsx
+++ src/components/Inicio/FrontPage.tsx
@@ -56,7 +56,7 @@ export default function FrontPage() {
       <div className=" flex-1 basis-[90%] px-4 max-w-6xl m-auto text-white text-center flex flex-col gap-10 items-center py-10 pb-15 md:px-8 sm:py-30 sm:flex-row sm:text-left sm:gap-12 md:gap-18 lg:gap-25 xl:px-0">
         <div className="flex flex-col gap-8 sm:w-1/3 lg:w-1/3">
           <h1 className={`text-4xl font-bold sm:text-4xl md:text-5xl lg:text-6xl text-balance duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`} style={{ textShadow: '0 0 1px currentColor, 1px 0 1px currentColor, 0 1px 1px currentColor, -1px 0 1px currentColor' }}>Tranformamos vidas con el poder de la educación</h1>
-          <p className={`sm:text-base md:text-xl lg:text-xl duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}>Brindamos talleres gratuitos, cursos en nuestra aula virtual y proyectos educativos con impacto solial para estudiantes y docentes.</p>
+          <p className={`sm:text-base md:text-xl lg:text-xl duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}>Brindamos talleres gratuitos, cursos en nuestra aula virtual y proyectos educativos con impacto social para estudiantes y docentes.</p>
           <div className="flex gap-5 w-fit m-auto sm:m-0 sm:relative sm:top-9 md:top-0">
             <Link to="donar">
               <button className={`group cursor-pointer whitespace-nowrap text-sm md:text-sm lg:text-lg border-2 border-white px-5 py-2 rounded-4xl bg-nd duration-1000 delay-300 hover:scale-105 hover:shadow-lg hover:shadow-nd/50 transition-all ease-out ${inView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>
