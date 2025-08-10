f04aa30d9039775cd42debd739593814e410b830
AcuacheY <v.acuache15@gmail.com>
2025-08-09 23:01:55 -0500

    feat(Error404): Delete import iconify

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Footer } from '../components'
-import { Icon } from '@iconify/react'
 
 export default function Error404() {
   useEffect(() => {
