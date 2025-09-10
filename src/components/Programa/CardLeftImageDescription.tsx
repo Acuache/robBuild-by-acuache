95ead2482e881ccf5f038f0629a2e1e4eb88883c
AcuacheY <v.acuache15@gmail.com>
2025-09-10 18:52:19 -0500

    feat(CardImageDescription): Added animations with React Intersection Observer

--- src/components/Programa/CardLeftImageDescription.tsx
+++ src/components/Programa/CardLeftImageDescription.tsx
@@ -1,16 +1,20 @@
+import { useInView } from 'react-intersection-observer'
+import { getDetailProgramImage } from '../../utils/importImage'
 interface CardLeftImageDescriptionProps {
   imageName: string
   titleImage: string
   descriptionImage: string
 }
-import { getDetailProgramImage } from '../../utils/importImage'
 export default function CardLeftImageDescription({ imageName, titleImage, descriptionImage }: CardLeftImageDescriptionProps) {
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+  })
   return (
-    <article className='w-full flex flex-col items-center lg:flex-row lg:relative  lg:px-8 xl:px-0 lg:-mt-25'>
-      <div className='lg:flex-2/3 max-w-192 lg:h-165'>
+    <article ref={ref} className='w-full flex flex-col items-center lg:flex-row lg:relative  lg:px-8 xl:px-0 lg:-mt-25'>
+      <div className={`lg:flex-2/3 max-w-192 lg:h-165 transition-all duration-1000 ${inView ? "opacity-100 -translate-x-0" : "opacity-0 -translate-x-80"}`}>
         <img className='w-full h-full object-cover' src={getDetailProgramImage(imageName)} alt="" />
       </div>
-      <div className='flex flex-col justify-center items-center p-7 gap-4 bg-white lg:absolute lg:h-80 lg:right-0 lg:top-1/2 lg:-translate-1/2 lg:-translate-x-0 lg:mt-0 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:text-start'>
+      <div className={`flex flex-col justify-center items-center p-7 gap-4 bg-white lg:absolute lg:h-80 lg:right-0 lg:top-1/2 lg:-translate-y-1/4 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:text-start transition-all duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-80"}`}>
         <h3 className='text-2xl  font-semibold sm:text-3xl'>{titleImage}</h3>
         <p className='text-md  sm:text-lg'>{descriptionImage}</p>
       </div>
--- src/components/Programa/CardRigthImageDescription.tsx
+++ src/components/Programa/CardRigthImageDescription.tsx
@@ -1,3 +1,4 @@
+import { useInView } from 'react-intersection-observer'
 interface CardRigthImageDescriptionProps {
   imageName: string
   titleImage: string
@@ -5,19 +6,22 @@ interface CardRigthImageDescriptionProps {
 }
 import { getDetailProgramImage } from '../../utils/importImage'
 export default function CardRigthImageDescription({ imageName, titleImage, descriptionImage }: CardRigthImageDescriptionProps) {
+  const { ref, inView } = useInView({
+    threshold: 0.3
+  })
   // Mejorar a futuro está barbaridad 5:55pm 10/09/2025 ¿Que tan grande seré a futuro?
   return (
-    <article className='w-full flex flex-col items-center lg:flex-row lg:relative  lg:px-8 xl:px-0 lg:justify-end lg:-mt-25'>
-      <div className='hidden lg:flex flex-col justify-center items-center p-7 gap-4 bg-red-700 lg:absolute lg:h-80 lg:left-0 lg:top-1/2 lg:-translate-1/2 lg:-translate-x-0 lg:mt-0 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:text-start'>
+    <article ref={ref} className='w-full flex flex-col items-center lg:flex-row lg:relative  lg:px-8 xl:px-0 lg:justify-end lg:-mt-25'>
+      <div className={`z-2 flex flex-col justify-center items-center p-7 gap-4 bg-white lg:absolute lg:h-80 lg:left-0 lg:top-1/2 lg:-translate-y-1/4 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:text-end transition-all duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-80"}`}>
         <h3 className='text-2xl  font-semibold sm:text-3xl'>{titleImage}</h3>
         <p className='text-md  sm:text-lg'>{descriptionImage}</p>
       </div>
 
-      <div className='lg:flex-2/3 max-w-192 lg:h-165'>
+      <div className={`lg:flex-2/3 max-w-192 lg:h-165 transition-all duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-80"}`}>
         <img className='w-full h-full object-cover' src={getDetailProgramImage(imageName)} alt="" />
       </div>
 
-      <div className='flex flex-col justify-center items-center p-7 gap-4 bg-white lg:absolute lg:h-80 lg:left-0 lg:top-1/2 lg:-translate-1/2 lg:-translate-x-0 lg:mt-0 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:text-end'>
+      <div className={`z-2 flex flex-col justify-center items-center p-7 gap-4 bg-white lg:absolute lg:h-80 lg:left-0 lg:top-1/2 lg:-translate-y-1/4 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:text-end transition-all duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-80"}`}>
         <h3 className='text-2xl  font-semibold sm:text-3xl'>{titleImage}</h3>
         <p className='text-md  sm:text-lg'>{descriptionImage}</p>
       </div>
