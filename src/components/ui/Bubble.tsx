62b3849c57ae318528d8c70e0d47a540d2059f43
AcuacheY <v.acuache15@gmail.com>
2025-09-14 13:19:07 -0500

    style(bubble): add bg img

--- src/components/ui/Bubble.tsx
+++ src/components/ui/Bubble.tsx
@@ -6,20 +6,20 @@ interface BubbleProps {
 export default function Bubble({ isView }: BubbleProps) {
   return (
     <section className={`absolute inset-0 ${!isView ? 'paused invisible' : ''}`}>
-      <article className="absolute size-20 rounded-full bg-white/15 shape1">
-        <img className='w-full h-full object-cover opacity-30 p-1' src={bubleRobBuild} />
+      <article className="absolute size-20 rounded-full bg-white/15 shape1 overflow-hidden opacity-50">
+        <img className='w-full h-full object-cover opacity-30' src={bubleRobBuild} />
       </article>
-      <article className="absolute size-15 rounded-full bg-white/10 shape2">
-        <img className='w-full h-full object-cover opacity-30 p-1' src={bubleRobBuild} />
+      <article className="absolute size-15 rounded-full bg-white/10 shape2 overflow-hidden opacity-50">
+        <img className='w-full h-full object-cover opacity-30' src={bubleRobBuild} />
       </article>
-      <article className="absolute size-5 rounded-full bg-white/20 shape3">
-        <img className='w-full h-full object-cover opacity-30 p-1' src={bubleRobBuild} />
+      <article className="absolute size-5 rounded-full bg-white/20 shape3 overflow-hidden opacity-50">
+        <img className='w-full h-full object-cover opacity-30 ' src={bubleRobBuild} />
       </article>
-      <article className="absolute size-15 rounded-full bg-white/20 shape4">
-        <img className='w-full h-full object-cover opacity-30 p-1' src={bubleRobBuild} />
+      <article className="absolute size-15 rounded-full bg-white/20 shape4 overflow-hidden opacity-50">
+        <img className='w-full h-full object-cover opacity-30 ' src={bubleRobBuild} />
       </article>
-      <article className="absolute size-8 rounded-full bg-white/20 shape5">
-        <img className='w-full h-full object-cover opacity-30 p-1' src={bubleRobBuild} />
+      <article className="absolute size-8 rounded-full bg-white/20 shape5 overflow-hidden opacity-50">
+        <img className='w-full h-full object-cover opacity-30' src={bubleRobBuild} />
       </article>
     </section>
   )
