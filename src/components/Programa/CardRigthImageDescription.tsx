307c2ae0ba3c1530d5bd07388bc07ffe7d12f1aa
AcuacheY <v.acuache15@gmail.com>
2025-09-14 14:41:31 -0500

    feat(api-detail-programs) :aggregation of real information

--- src/components/Programa/CardRigthImageDescription.tsx
+++ src/components/Programa/CardRigthImageDescription.tsx
@@ -12,16 +12,16 @@ export default function CardRigthImageDescription({ imageName, titleImage, descr
   })
   return (
     <article ref={ref} className='w-full flex flex-col items-center lg:flex-row lg:relative  xl:px-0 lg:justify-end lg:-mt-25'>
-      <div className={`z-2 hidden lg:flex flex-col justify-center items-center p-7 gap-4 bg-white lg:absolute lg:h-80 lg:left-0 lg:top-1/2 lg:-translate-y-1/4 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:text-end transition-all duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-80"}`}>
+      <div className={`z-2 hidden lg:flex flex-col justify-center items-center p-7 gap-4 bg-white lg:absolute lg:h-80 lg:left-0 lg:top-1/2 lg:-translate-y-1/4 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:items-end lg:text-end transition-all duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-80"}`}>
         <h3 className='text-2xl  font-semibold sm:text-3xl'>{titleImage}</h3>
         <p className='text-md  sm:text-lg'>{descriptionImage}</p>
       </div>
 
-      <div className={`lg:flex-2/3 max-w-160 lg:h-165 transition-all duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-80"}`}>
+      <div className={`w-full h-100 sm:h-120 lg:flex-2/3 max-w-160  lg:h-150 transition-all duration-1000 overflow-hidden ${inView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-80"}`}>
         <img className='w-full h-full object-cover' src={getDetailProgramImage(imageName)} alt="" />
       </div>
 
-      <div className={`z-2 flex flex-col justify-center items-center p-7 gap-4 bg-white lg:absolute  lg:left-0 lg:top-1/2 lg:-translate-y-1/4 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:text-end transition-all duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-80"}`}>
+      <div className={`z-2 flex flex-col justify-center items-center p-7 gap-4 bg-white lg:absolute  lg:left-0 lg:top-1/2 lg:-translate-y-1/4 sm:p-12 sm:max-w-115 sm:-mt-20 lg:flex-1/3 text-center lg:text-end lg:items-end transition-all duration-1000 ${inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-80"}`}>
         <h3 className='text-2xl  font-semibold sm:text-3xl'>{titleImage}</h3>
         <p className='text-md  sm:text-lg'>{descriptionImage}</p>
       </div>
