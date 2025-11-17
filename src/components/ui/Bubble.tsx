8f793d578dc970628d26836107a622d26d08d11b
AcuacheY <v.acuache15@gmail.com>
2025-11-16 20:26:03 -0500

    style(Bubble): add select-none and draggable attributes to images for improved user experience

--- src/components/ui/Bubble.tsx
+++ src/components/ui/Bubble.tsx
@@ -6,20 +6,20 @@ interface BubbleProps {
 export default function Bubble({ isView }: BubbleProps) {
   return (
     <section className={`absolute inset-0 ${!isView ? 'paused invisible' : ''}`}>
-      <article className="absolute size-20 rounded-full bg-white/15 shape1 overflow-hidden opacity-50">
-        <img className='w-full h-full object-cover opacity-30' src={bubleRobBuild} alt="Bubble number 1" />
+      <article className="absolute size-20 rounded-full bg-white/15 shape1 overflow-hidden opacity-50 select-none">
+        <img className='w-full h-full object-cover opacity-30 select-none' draggable="false" src={bubleRobBuild} alt="Bubble number 1" />
       </article>
-      <article className="absolute size-15 rounded-full bg-white/10 shape2 overflow-hidden opacity-50">
-        <img className='w-full h-full object-cover opacity-30' src={bubleRobBuild} alt="Bubble number 2" />
+      <article className="absolute size-15 rounded-full bg-white/10 shape2 overflow-hidden opacity-50 select-none">
+        <img className='w-full h-full object-cover opacity-30 select-none' draggable="false" src={bubleRobBuild} alt="Bubble number 2" />
       </article>
-      <article className="absolute size-5 rounded-full bg-white/20 shape3 overflow-hidden opacity-50">
-        <img className='w-full h-full object-cover opacity-30 ' src={bubleRobBuild} alt="Bubble number 3" />
+      <article className="absolute size-5 rounded-full bg-white/20 shape3 overflow-hidden opacity-50 select-none">
+        <img className='w-full h-full object-cover opacity-30 select-none' draggable="false" src={bubleRobBuild} alt="Bubble number 3" />
       </article>
-      <article className="absolute size-15 rounded-full bg-white/20 shape4 overflow-hidden opacity-50">
-        <img className='w-full h-full object-cover opacity-30 ' src={bubleRobBuild} alt="Bubble number 4" />
+      <article className="absolute size-15 rounded-full bg-white/20 shape4 overflow-hidden opacity-50 select-none">
+        <img className='w-full h-full object-cover opacity-30 select-none  draggable="false"' src={bubleRobBuild} alt="Bubble number 4" />
       </article>
-      <article className="absolute size-8 rounded-full bg-white/20 shape5 overflow-hidden opacity-50">
-        <img className='w-full h-full object-cover opacity-30' src={bubleRobBuild} alt="Bubble number 5" />
+      <article className="absolute size-8 rounded-full bg-white/20 shape5 overflow-hidden opacity-50 select-none">
+        <img className='w-full h-full object-cover opacity-30 select-none' draggable="false" src={bubleRobBuild} alt="Bubble number 5" />
       </article>
     </section>
   )
